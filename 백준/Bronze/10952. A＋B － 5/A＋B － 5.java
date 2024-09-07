9fcf813be8478254fda3bc6847aa25902f07cf9e
esc <93801149+esc-beep@users.noreply.github.com>
2024-09-07 22:41:35 +0900

    [Bronze V] Title: A+B - 5, Time: 180 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			int A = in.nextInt();
+			int B = in.nextInt();
+			if(A == 0 && B == 0) break;
+			System.out.println(A+B);
+		}
+		in.close();
+	}
+}
