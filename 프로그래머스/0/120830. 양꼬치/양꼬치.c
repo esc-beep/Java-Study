b5b78917dc51ce110e089658692af5950b1c51cf
esc <93801149+esc-beep@users.noreply.github.com>
2024-09-11 22:12:19 +0900

    Delete 프로그래머스/0/120830. 양꼬치/양꼬치.c

--- 프로그래머스/0/120830. 양꼬치/양꼬치.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int n, int k) {
-    int answer = 0;
-    answer = n * 12000 + (k - n / 10) * 2000;
-    return answer;
-}
