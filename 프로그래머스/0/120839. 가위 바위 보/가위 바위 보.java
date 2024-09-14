ad3bb415012627da0d45c5fcf3c6fab6ca81882a
esc <93801149+esc-beep@users.noreply.github.com>
2024-09-14 14:58:56 +0900

    [level 0] Title: 가위 바위 보, Time: 2.59 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        for(char x : rsp.toCharArray()) {
+            if (x == '2') {
+                answer += "0";
+            } else if (x == '0') {
+                answer += "5";
+            } else answer += "2";
+        }
+        return answer;
+    }
+}
